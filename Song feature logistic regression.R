56944946b3a5c67d34f4122674dd974952ee5b46
eblackburn4 <eblackburn4@sheffield.ac.uk>
2024-12-11 14:39:34 +0000

    Creation of second r script for second part of final project

---
+++ Song feature logistic regression.R
@@ -0,0 +1,12 @@
+
+## ---------------------------
+## Purpose of script: Intro to data science final project part II: Song feature logistic regression
+## Author: Ned Blackburn
+## Date Created: 2024-12-11
+
+options(scipen = 6, digits = 5) 
+library(tidyverse)
+library(hrbrthemes)
+library(glmnet)
+
+## ---------------------------
--- Song lyric sentiment analysis.R
+++ Song lyric sentiment analysis.R
@@ -1,5 +1,6 @@
 ## ---------------------------
-## Purpose of script: Intro to data science final project: R Code
+## Purpose of script: Intro to data science final project part I: R Code for sentiment analysis 
+## of hit song lyrics and comparison to nob hits
 ## Author: Ned Blackburn
 ## Date Created: 2024-11-27
 
@@ -8,13 +9,17 @@ library(tidyverse)
 library(hrbrthemes)
 library(GGally)
 library(ggfortify)
+library(tidytext)
 
 setwd("~/Desktop/Intro to DS:data vis")
 
 ## ------------------------------------------------------------------------
-# Part 1: sentiment analysis of lyrics ------------------------------------
+# RQ1: how has the lyrical sentiment of popular songs changed over time?
+# RQ2: is there a relationship between lyrical sentiment and musical 'valence'?
 ## ------------------------------------------------------------------------
-# RQ: what is the relationship between lyric sentiment and song popularity?
+
+
+# 1. Reading in data ------------------------------------------------------
 
 #read in data for lyrics, track metadata (for release date) and artist metadata (for genre), and drop unneeded columns
 
@@ -23,14 +28,13 @@ track_meta <- read.csv("Data/musicoset_metadata/tracks.csv", sep = "\t") |>
   
 song_lyrics <- read.csv("Data/musicoset_songfeatures/lyrics.csv", sep = "\t")
 
-artist_meta <- read_delim(
-  'Data/musicoset_metadata/artists.csv',
+artist_meta <- read_delim('Data/musicoset_metadata/artists.csv',
   delim = "\t",          # Main delimiter seems to be tabs based on a visual inspection
   escape_double = TRUE,  # Handle stray quotes
   col_names = TRUE,
   trim_ws = TRUE
 )
-#read in data for song information (for popularity). The CSV isn't formatted properly so will require additional cleaning steps
+#read in data for song information (for popularity). This particular CSV isn't formatted properly so will require additional cleaning steps
 
 song_meta <- read_delim(
   'Data/musicoset_metadata/songs.csv',
@@ -60,6 +64,8 @@ song_meta <- song_meta |>
            extra = "merge") |>
   select(!c('billboard','explicit'))
 
+
+
 #check for na values - all checks return 0 so we're good to proceed
 sum(is.na(song_meta))
 sum(is.na(song_lyrics))
@@ -70,8 +76,19 @@ song_master <- left_join(song_meta, track_meta, by = 'song_id') |>
   left_join(song_lyrics, by = 'song_id') |>
   filter(lyrics != "")
 
-# Data preprocessing and EDA ----------------------------------------------
+# 2.Data preprocessing and EDA --------------------------------------------
 
+#remove meta-words and linebreaks in the the lyrics column that are not actually part of the lyrics e.g. 'verse 1'
+
+unwantedtext <- c('Verse','Chorus', 'Intro','1','2','3','4')
+texttest <- song_master |>
+  mutate(lyrics = str_remove_all(lyrics, str_c(unwantedtext, collapse = "|"))) |>
+  mutate(lyrics = str_replace_all(lyrics, "\\\\n", ""))
+
+#use tidytext's 'unnest tokens' function to split each word into its own row
+
+texttest <- texttest |>
+  unnest_tokens(word, lyrics)
 
 
 
